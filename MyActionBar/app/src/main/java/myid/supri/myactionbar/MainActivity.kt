0d37150fe05e14513208a8ff13220577b4874ea3
supriyanto4id <supriprosesor@gmail.com>
2022-02-16 23:07:49 +0800

    My Action bar

--- MyActionBar/app/src/main/java/myid/supri/myactionbar/MainActivity.kt
+++ MyActionBar/app/src/main/java/myid/supri/myactionbar/MainActivity.kt
@@ -1,11 +1,70 @@
 package myid.supri.myactionbar
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import android.widget.Toast
+import myid.supri.myactionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater =menuInflater
+        inflater.inflate(R.menu.option_menu,menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint =resources.getString(R.string.search)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       when(item.itemId){
+           R.id.menu1->{
+               supportFragmentManager.beginTransaction()
+                       .replace(R.id.fragment_container,MenuFragment())
+                       .addToBackStack(null)
+                       .commit()
+               return false
+           }
+           R.id.menu2 -> {
+               val i = Intent(this, MenuActivity::class.java)
+               startActivity(i)
+               return true
+       }
+           else ->return true
+    }
+}
 }
